17905c9bfdc6aa1987a3fa699893011040b33365
Tyler <tyler73750@gmail.com>
2023-03-08 20:51:15 -0600

    Changed project to vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <square v-bind:xPt=0 v-bind:yPt=0></square>
+  </div>
+
+  <div>
+    <p>Current dimensions of grid: {{ x }} by {{ y }}</p>
+    <input type="text" v-model.lazy="dimensions" placeholder="Enter two numbers: x y">
+    <input type="submit" @click="onClick">
+  </div>
+</template>
+
+<script setup lang="ts">
+import square from '@/components/square.vue'
+import { ref } from 'vue'
+
+const dimensions = ref<string>("")
+var x = ref<number>(2)
+var y = ref<number>(2)
+
+// TODO: Add error handling for invalid input
+const onClick = () => {
+  // Parser dimensions into x and y
+  x.value = parseInt(dimensions.value.split(" ")[0])
+  y.value = parseInt(dimensions.value.split(" ")[1])
+}
+
+</script>
+
---
+++ vue-project/src/components/square.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{ xPt }}{{ yPt }}</div>
+</template>
+
+
+<script setup lang="ts">
+
+const props = defineProps<{
+     xPt: number,
+     yPt: number
+    }>()
+
+
+</script>
