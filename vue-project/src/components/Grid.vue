ee414140a8c547b69355a14726a668bc0e8e01e7
ROMANT21 <tyler73750@gmail.com>
2023-03-11 01:58:34 -0800

    Added obstacles, a goal, and simulated keypress

--- vue-project/src/components/Grid.vue
+++ vue-project/src/components/Grid.vue
@@ -1,16 +1,40 @@
 <template>
-    <div v-for="col in props.dim" class="grid" :key="componentKey">
-        <div v-for="row in props.dim">
-            <div class="space" v-if="positionGrid[props.dim - row][col-1] == true">
-                <img class="agent" src="@/assets/corgi.png">
-            </div>
-            <div class="space" v-else></div>
-        </div>   
+
+    <div class="container">
+        <div v-for="col in props.dim" class="grid" :key="componentKey">
+            <div v-for="row in props.dim">
+                <div class="space" >
+                    <img v-if="grid[col-1][props.dim - row] == 1" class="agent" src="@/assets/agent.png">
+                    <img v-if="grid[col-1][props.dim - row] == 2" class="obstacle" src="@/assets/obstacle.jfif">
+                    <img v-if="grid[col-1][props.dim - row] == 3" class="goal" src="@/assets/goal.png">
+                </div>
+            </div>   
+        </div>
     </div>
 
+    <button @click="sendCommands(['ArrowUp', 'ArrowUp', 'ArrowUp', 'ArrowRight', 'ArrowRight', 'ArrowRight'])"></button>
 </template>
 <style>
-/* TODO: FIX CONTAINER SHIFTING */
+.container{
+    display: flex;
+}
+
+.settings{
+
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+    margin-top: 20px;
+}
+
+.button{
+    display: flex;
+    position: relative;
+
+}
+
+/* TODO: FIX GRID SHIFTING */
 .grid{
     display: inline-grid;
 }
@@ -26,57 +50,127 @@
 .agent{
 
     width: 200%;
+    height: 203%;
+}
+
+.goal{
+    width: 200%;
+    height: 203%;
+}
+
+.obstacle{
+    width: 200%;
     height: 200%;
 }
+
 </style>
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import Square from '@/components/Square.vue'
+import { ref, onMounted, resolveComponent } from 'vue'
 
+
+/* 
+VALUES FOR GRID:
+0 = empty
+1 = agent
+2 = obstacle
+3 = goal
+*/ 
+
+// Dimensions of Grid passed from parent component
 const props = defineProps<{
      dim: number,
 }>()
 
-var componentKey = ref(0)
+
+// Create grid (dim by dim) with all values set to 0 (empty)
 const grid: number[][] = new Array<Array<number>>
 for (let row = 0; row < props.dim; ++row){
     grid[row] = new Array<number>
     for (let col = 0; col < props.dim; ++col){
-        grid[row][col] = col
+        grid[row][col] = 0
     }
 }
 
-var positionGrid: boolean[][] = new Array<Array<boolean>>
+// Set default agent, goal, and obstacle states
 var currPos: number[] = [0, 0]
-for (let row = 0; row < props.dim; ++row){
-    positionGrid[row] = new Array<boolean>
-    for (let col = 0; col < props.dim; ++col){
-        positionGrid[row][col] = false
-    }
-}
-positionGrid[currPos[0]][currPos[1]] = true
+grid[currPos[0]][currPos[1]] = 1        // Set agent state
+grid[props.dim - 1][props.dim - 1] = 3  // Set goal state
+grid[1][1] = 2                          // Set obstacle state
 
-// Check for when agent moves through user
+// Move Agent, depending on input
+var componentKey = ref(0)   // Used to force re-render
 window.addEventListener('keydown', (e) => {
-    positionGrid[currPos[0]][currPos[1]] = false
+    let nextPos: number[] = [currPos[0], currPos[1]]
     // Agent moved up one square
     if (e.key == 'ArrowUp'){
-        currPos[0] += 1
+        nextPos[1] += 1
     }
     if (e.key == 'ArrowDown'){
-        currPos[0] -= 1
+        nextPos[1] -= 1
     }
     if (e.key == 'ArrowRight'){
-        currPos[1] += 1
+        nextPos[0] += 1
     }
     if (e.key == 'ArrowLeft'){
-        currPos[1] -= 1
+        nextPos[0] -= 1
     }
 
-    positionGrid[currPos[0]][currPos[1]] = true
-        componentKey.value += 1;
-    console.log(positionGrid)
-    console.log(currPos[1])
+    // Check if agent is out of bounds or hit obstacle
+    if (!isPassable(nextPos)){
+        return
+    }
+
+    // Check if agent is at goal state
+    if (isGoalState(nextPos)){
+        console.log("GOAL STATE REACHED")
+    }
+
+    // Move agent to new position
+    grid[currPos[0]][currPos[1]] = 0
+    currPos[0] = nextPos[0]
+    currPos[1] = nextPos[1]
+    grid[currPos[0]][currPos[1]] = 1
+    componentKey.value += 1;
 })
 
+function isPassable(pos: number[]){
+    if (pos[0] < 0 || pos[1] < 0 ||
+        pos[0] >= props.dim || pos[1] >= props.dim ||
+        grid[pos[0]][pos[1]] == 2){
+        return false
+    }
+    return true
+}
+
+function isGoalState(pos: number[]) {
+    if (grid[pos[0]][pos[1]] == 3){
+        return true
+    }
+    return false
+}
+
+function simulateKeyPress (key: string) {
+    // Create keydown event
+    return new Promise(resolve =>{
+    var event = new KeyboardEvent('keydown', {
+            key: key,
+            });
+        window.dispatchEvent(event);
+        console.log(key)
+    setTimeout(() => {resolve('')}, 500)
+    })
+}
+
+async function sendCommands(commands: string[]) {
+    // Simulate key presses
+    for (let i = 0; i < commands.length; ++i){
+        await simulateKeyPress(commands[i])
+    }
+}
+
+function depthFirstSearchAlg(currPos: number[], goalPos: number[], grid: number[][], visited: boolean[][], path: string[]) {
+}
+
+function breadthFirstSearchAlg(currPos: number[], goalPos: number[], grid: number[][], visited: boolean[][], path: string[]) {
+}
 </script>
